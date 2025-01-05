03144c019925186fdae035c2168891c843688505
Anik Banerji <anikkbanerji@gmail.com>
2025-01-05 13:41:50 -0600

    meepmeep stuff

--- MeepMeepTesting/src/main/java/com/suman/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/suman/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.suman.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -14,18 +15,18 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(23, -52, Math.toRadians(90)))
+                .splineTo(new Vector2d(0,-35),Math.toRadians(90))
+                //wait ant put speciamskdfjas;dlfkja onto the cage
+                .waitSeconds(2)
+                .lineToYLinearHeading(-38,Math.toRadians(-90))
+                .splineTo(new Vector2d(48,-38),Math.toRadians(90))
+//                        .splineTo(new Vector2d(0,-35),Math.toRadians(-90))
+//                .splineTo(new Vector2d(Vector2d37.5,-25),Math.toRadians(0))
+                //do intake
                 .build());
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
